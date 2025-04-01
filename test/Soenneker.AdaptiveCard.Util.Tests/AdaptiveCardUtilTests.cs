2d88627f9f1d105252220c4041d63e7bea154870
Jake Soenneker <jake@soenneker.com>
2025-04-01 18:53:42 -0500

    test

--- test/Soenneker.AdaptiveCard.Util.Tests/AdaptiveCardUtilTests.cs
+++ test/Soenneker.AdaptiveCard.Util.Tests/AdaptiveCardUtilTests.cs
@@ -1,5 +1,8 @@
-﻿using Soenneker.AdaptiveCard.Util.Abstract;
+﻿using FluentAssertions;
+using Soenneker.AdaptiveCard.Util.Abstract;
+using Soenneker.Enums.JsonLibrary;
 using Soenneker.Tests.FixturedUnit;
+using Soenneker.Utils.Json;
 using Xunit;
 
 namespace Soenneker.AdaptiveCard.Util.Tests;
@@ -18,5 +21,17 @@ public class AdaptiveCardUtilTests : FixturedUnitTest
     public void Default()
     {
 
+    }
+
+    [Fact]
+    public void AdaptiveCard_should_serialize_and_deserialize_with_newtonsoft()
+    {
+        AdaptiveCards.AdaptiveCard adaptiveCard = _util.Build(Faker.Commerce.ProductName());
+
+        string serialized = JsonUtil.Serialize(adaptiveCard, libraryType: JsonLibraryType.Newtonsoft)!;
+
+        var result = JsonUtil.Deserialize<AdaptiveCards.AdaptiveCard>(serialized, JsonLibraryType.Newtonsoft);
+
+        result.Should().NotBeNull();
     }
 }
